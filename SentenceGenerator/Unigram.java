9313635cef555279d06c303510e1137a59df705b
ksayers91 <sayers2@tcnj.edu>
2016-11-21 22:05:21 -0500

    Update Unigram.java

--- SentenceGenerator/Unigram.java
+++ SentenceGenerator/Unigram.java
@@ -1,6 +1,9 @@
 import java.util.HashMap;
 import java.util.Map;
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Unigram Class. This is a data structure that is assigned to each unique unigram in the input file
+//Each Unigram object is composed of the word, count of appearances of the word, and a Map object that refrences every
+//Bigram pair with this unigram as the first word of the bigram.
 public class Unigram implements Comparable<Unigram>{
 	
 	String word;
@@ -19,22 +22,25 @@ public class Unigram implements Comparable<Unigram>{
 		bigramCounts.put(arr[1], ct);
 		count += ct;
 	}
+//Classes allow for sorting
     public int compareTo(Unigram p) {
        // return (this.count).compareTo(p.count);
         		//compareTo(p.count);
     	return word.compareTo(p.word);
     }
-
+//Classes allow for sorting
     public boolean equals(Object p) {
     	Unigram temp;
     	temp = (Unigram) p;
         return word.equals(temp.word);
     }
-    
+    //Used for debugging
     public String toString () {
     	return "Unigram: " + word + "\tCount: " + count;
     }
     
+//This is responsible for picking and choosing the next word occurance in a bulding sentence.
+//It is a weighted lottery that favors the more common appearances.
     public String generateLotteryWord() {
     	int sum = 0;
     	int lot = (int)(Math.random()*count) + 1;
@@ -49,7 +55,8 @@ public class Unigram implements Comparable<Unigram>{
 	    }
     	return "";
     }
-    
+
+//Used in debugging
     public void printAssociatedBigrams() {
     	for (Object key : bigramCounts.keySet()) {
     		key = (String) key;
